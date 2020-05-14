5d40df0ca27fa995a490be796707c29cc05a4f08
nn143 <57165323+nn143@users.noreply.github.com>
2020-05-14 11:43:49 +0400

    Add files via upload

---
+++ PT3/src/Vehicle.java
@@ -0,0 +1,14 @@
+
+public class Vehicle {
+	
+	int ChassisNo;
+	int ModelYear; 
+	String make; 
+	
+	public Vehicle(int a, int b, String c ) { // constructor
+		this.ChassisNo = a; 
+		this.ModelYear = b; 
+		this.make = c; 
+	}
+
+}
